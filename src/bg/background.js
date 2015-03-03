73012b36b9f9fc175d48369f4bcdba37b29e38f5
J Delaney <j.delaney.sd@gmail.com>
2015-03-02 19:53:36 -0800

    Made an HTTP request from an HTTPS site!

--- src/bg/background.js
+++ src/bg/background.js
@@ -5,6 +5,24 @@ chrome.extension.onMessage.addListener(
     sendResponse();
   });
 
-chrome.extension.onRequest.addListener(function (request, sender, callback) {
+chrome.runtime.onMessage.addListener(function(request, sender, callback) {
+    if (request.action == "xhttp") {
+        var xhttp = new XMLHttpRequest();
+        var method = request.method ? request.method.toUpperCase() : 'GET';
 
+        xhttp.onload = function() {
+            callback(xhttp.responseText);
+        };
+        xhttp.onerror = function() {
+            // Do whatever you want on error. Don't forget to invoke the
+            // callback to clean up the communication port.
+            callback();
+        };
+        xhttp.open(method, request.url, true);
+        if (method == 'POST') {
+            xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        }
+        xhttp.send(request.data);
+        return true; // prevents the callback from being called too early on return
+    }
 });
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,12 +7,67 @@ function parseQuery(query) {
         params[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
     }
 
-    return params
+    return params;
+}
+
+function parseCourseID(courseID) {
+    var matches = courseID.match(/([A-Za-z]*)(.*)/);
+    if (matches && matches.length === 3) {
+        return [matches[1], matches[2]];
+    } else {
+        return false;
+    }
+}
+
+function parseTeacherName(teacher) {
+    var matches = teacher.match(/(\w*),\s(\w*)/);
+    if (!matches || matches.length < 3) {
+        return false;
+    }
+    return {
+        fullname: teacher,
+        nomiddle: matches[1] + ', ' + matches[2],
+        fname: matches[2],
+        lname: matches[1]
+    };
+}
+
+function getGradeDistribution(teacher, subjectCode, courseCode) {
+    var url = 'http://asucsd.ucsd.edu/gradeDistribution?' +
+        'GradeDistribution%5BTERM_CODE%5D=' +
+        '&GradeDistribution%5BSUBJECT_CODE%5D='+ subjectCode +
+        '&GradeDistribution%5BCOURSE_CODE%5D=' + courseCode +
+        '&GradeDistribution%5BCRSE_TITLE%5D=' +
+        '&GradeDistribution%5BINSTRUCTOR%5D=' + encodeURIComponent(teacher.nomiddle) +
+        '&GradeDistribution%5BGPA%5D=' +
+        '&GradeDistribution_page=1' +
+        '&ajax=gradedistribution-grid';
+
+    chrome.runtime.sendMessage({
+        method: 'GET',
+        action: 'xhttp',
+        url: url
+    }, function(responseText) {
+        console.log(responseText);
+    });
 }
 
 $(document).ready(function () {
     $('#listing').on('click', 'a', function (event) {
         var a = $(event.target)[0];
-        
+        var params = parseQuery(a.search);
+
+        if ('sectionletter' in params) { //Viewing a teacher
+            var teacher = $('#listing').find('ul.courseinfo').children('li').first().text();
+            teacher = parseTeacherName(teacher);
+            var result = parseCourseID(params['courseid']);
+            if (!result || result.length !== 2) {
+                return console.error('Problem parsing course ID: ' + params);
+            }
+            var subjectCode = result[0];
+            var courseCode = result[1];
+
+            getGradeDistribution(teacher, subjectCode, courseCode);
+        }
     });
 });

213a7e781808309ca1c773729a14c4dbae3ffdb7
J Delaney <j.delaney.sd@gmail.com>
2015-03-03 08:35:24 -0800

    Refactor into functions

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -32,11 +32,29 @@ function parseTeacherName(teacher) {
     };
 }
 
-function getGradeDistribution(teacher, subjectCode, courseCode) {
+function findTeacherName() {
+    var teacher = $('#listing').find('ul.courseinfo').children('li').first().text();
+    return parseTeacherName(teacher);
+}
+
+function findClass(params) {
+    var result = parseCourseID(params['courseid']);
+    if (!result || result.length !== 2) {
+        console.error('Problem parsing course ID: ' + params);
+        return false;
+    }
+
+    return {
+        subjectCode: result[0],
+        courseCode: result[1]
+    }
+}
+
+function getGradeDistribution(teacher, course) {
     var url = 'http://asucsd.ucsd.edu/gradeDistribution?' +
         'GradeDistribution%5BTERM_CODE%5D=' +
-        '&GradeDistribution%5BSUBJECT_CODE%5D='+ subjectCode +
-        '&GradeDistribution%5BCOURSE_CODE%5D=' + courseCode +
+        '&GradeDistribution%5BSUBJECT_CODE%5D='+ course.subjectCode +
+        '&GradeDistribution%5BCOURSE_CODE%5D=' + course.courseCode +
         '&GradeDistribution%5BCRSE_TITLE%5D=' +
         '&GradeDistribution%5BINSTRUCTOR%5D=' + encodeURIComponent(teacher.nomiddle) +
         '&GradeDistribution%5BGPA%5D=' +
@@ -51,7 +69,19 @@ function getGradeDistribution(teacher, subjectCode, courseCode) {
         var parser = new DOMParser();
         var htmlDoc = parser.parseFromString(html, "text/html");
         var page = $(htmlDoc);
-        console.log(page.find('#gradedistribution-grid'));
+        var tableRows = page.find('#gradedistribution-grid').children('table.items').children('tbody').children();
+
+        tableRows.each(function (i, elem) {
+            var cells = $(elem).children();
+            var gpa = $(cells[5]).text();
+            var aPercent = $(cells[6]).text();
+            var bPercent = $(cells[7]).text();
+            var cPercent = $(cells[8]).text();
+            var dPercent = $(cells[9]).text();
+            var fPercent = $(cells[10]).text();
+            var wPercent = $(cells[11]).text();
+            console.log(gpa, aPercent, bPercent, cPercent, dPercent, fPercent, wPercent);
+        });
     });
 }
 
@@ -61,16 +91,10 @@ $(document).ready(function () {
         var params = parseQuery(a.search);
 
         if ('sectionletter' in params) { //Viewing a teacher
-            var teacher = $('#listing').find('ul.courseinfo').children('li').first().text();
-            teacher = parseTeacherName(teacher);
-            var result = parseCourseID(params['courseid']);
-            if (!result || result.length !== 2) {
-                return console.error('Problem parsing course ID: ' + params);
-            }
-            var subjectCode = result[0];
-            var courseCode = result[1];
+            var teacher = findTeacherName();
+            var course = findClass(params);
 
-            getGradeDistribution(teacher, subjectCode, courseCode);
+            getGradeDistribution(teacher, course);
         }
     });
 });

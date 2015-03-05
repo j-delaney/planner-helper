d4e9d5b8a58b6c997876a7d50d8a337b3527207d
J Delaney <j.delaney.sd@gmail.com>
2015-03-05 15:58:34 -0800

    Fixed bug where no results would cause issues for CAPEs

--- src/inject/gradeDist.js
+++ src/inject/gradeDist.js
@@ -19,16 +19,20 @@ function getGradeDistribution(teacher, course, callback) {
         var page = $(htmlDoc);
         var tableRows = page.find('#gradedistribution-grid').children('table.items').children('tbody').children();
 
-        tableRows.each(function (i, elem) {
-            var termCode = $(cells[0]).text(); //e.g. SP13
-            var gpa = $(cells[5]).text();
-            var aPercent = $(cells[6]).text();
-            var bPercent = $(cells[7]).text();
-            var cPercent = $(cells[8]).text();
-            var dPercent = $(cells[9]).text();
-            var fPercent = $(cells[10]).text();
-            var wPercent = $(cells[11]).text();
-            console.log(gpa, aPercent, bPercent, cPercent, dPercent, fPercent, wPercent);
-        });
+        if (tableRows.first().children().first().hasClass('empty')) {
+            console.log('No results found');
+        } else {
+            tableRows.each(function (i, cells) {
+                var termCode = $(cells[0]).text(); //e.g. SP13
+                var gpa = $(cells[5]).text();
+                var aPercent = $(cells[6]).text();
+                var bPercent = $(cells[7]).text();
+                var cPercent = $(cells[8]).text();
+                var dPercent = $(cells[9]).text();
+                var fPercent = $(cells[10]).text();
+                var wPercent = $(cells[11]).text();
+                console.log(gpa, aPercent, bPercent, cPercent, dPercent, fPercent, wPercent);
+            });
+        }
     });
 }

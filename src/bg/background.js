a5fd0191c4086441d9aeacd33c298043b55e4b31
J Delaney <j.delaney.sd@gmail.com>
2015-04-01 17:30:08 -0700

    Removed unused onMessage listener. It was a leftover from the initial extension boilerplate

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,10 +1,3 @@
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
-
 chrome.runtime.onMessage.addListener(function(request, sender, callback) {
     if (request.action == "xhttp") {
         var xhttp = new XMLHttpRequest();

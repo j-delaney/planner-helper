b536b51b6f09fe8cdaa503b9a96f8b2e12da5c4c
J Delaney <j.delaney.sd@gmail.com>
2015-04-07 14:01:51 -0700

    Updated CAPE to use new Data Section style

--- src/inject/cape.js
+++ src/inject/cape.js
@@ -1,79 +1,38 @@
-var cape = null;
-
-function makeCapeSection() {
-    cape = $(
-        '<div class="section">' +
-            '<h3>CAPE</h3>' +
-            '<div id="cape">' +
-                '<div class="yes-data">' +
-                    '<ul>' +
-                        '<li><strong>Enrolled: </strong><span id="cape-enrolled"></span></li>' +
-                        '<li><strong>Evals Made: </strong><span id="cape-evals"></span></li>' +
-                        '<li><strong>Recommend Class: </strong><span id="cape-rcmd-class"></span></li>' +
-                        '<li><strong>Recommend Instructor: </strong><span id="cape-rcmd-instr"></span></li>' +
-                        '<li><strong>Study Hours/Week</strong><span id="cape-study-hrs"></span></li>' +
-                    '</ul>' +
-                '</div>' +
-                '<div class="no-data">No data could be found for this professor</div>' +
-                '<div class="loading-data">Loading data...</div>' +
-            '</div>' +
-        '</div>'
-    );
-
-    return cape;
+function Cape() {
+    DataSection.call(this, 'CAPE', 'cape', [
+        {label: 'Enrolled', dataField: 'enrolled'},
+        {label: 'Evals Made', dataField: 'evalsMade'},
+        {label: 'Recommend Class', dataField: 'recommendClass'},
+        {label: 'Recommend Instructor', dataField: 'recommendInstructor'},
+        {label: 'Study Hours/Week', dataField: 'studyHours'}
+    ]);
 }
 
-function formatCape(data) {
-    cape.find('.loading-data').slideUp(500);
-
-    if (data) {
-        cape.find('#cape-enrolled').text(data.enroll);
-        cape.find('#cape-evals').text(data.evalsMade);
-        cape.find('#cape-rcmd-class').text(data.recommendClass);
-        cape.find('#cape-rcmd-instr').text(data.recommendInstructor);
-        cape.find('#cape-study-hrs').text(data.studyHours);
-        cape.find('.yes-data').slideDown(500);
-
-        cape.find('h3').html('<a href="'+data.url+'" target="_blank">CAPE</a>');
-    } else {
-        cape.find('h3').html('CAPE');
-        cape.find('.no-data').slideDown(500);
-    }
-}
-
-function getCape(teacher, course, callback) {
+Cape.prototype.getNewData = function (teacher, course, callback) {
     var url = 'http://cape.ucsd.edu/responses/Results.aspx?' +
         'Name=' + encodeURIComponent(teacher.nomiddle)+
         '&CourseNumber=' + course.subjectCode + course.courseCode;
 
-    chrome.runtime.sendMessage({
-        method: 'GET',
-        action: 'xhttp',
-        url: url
-    }, function(html) {
-        var parser = new DOMParser();
-        var htmlDoc = parser.parseFromString(html, "text/html");
-        var page = $(htmlDoc);
-
-        if (page.find('#ctl00_ContentPlaceHolder1_gvCAPEs_ctl01_lblEmptyData').length) {
-            formatCape(null);
-            callback();
-        } else {
-            var result = page.find('#ctl00_ContentPlaceHolder1_gvCAPEs').children('tbody').children().first().children();
-
-            var data = {
-                enroll: $(result[3]).text(),
-                evalsMade: $(result[4]).text(),
-                recommendClass: $(result[5]).text(),
-                recommendInstructor: $(result[6]).text(),
-                studyHours: $(result[7]).text(),
-                avgGradeExpected: $(result[8]).text(),
-                avgGradeReceived: $(result[9]).text(),
-                url: url
-            };
-
-            formatCape(data);
-            callback();
+    this.fetchHTML(url, function (page) {
+        //If no page object or there are no results to show
+        if (!page || page.find('#ctl00_ContentPlaceHolder1_gvCAPEs_ctl01_lblEmptyData').length) {
+            this.data = null;
+            return callback();
         }
+
+        var result = page.find('#ctl00_ContentPlaceHolder1_gvCAPEs').children('tbody').children().first().children();
+
+        this.data = {
+            enroll: $(result[3]).text(),
+            evalsMade: $(result[4]).text(),
+            recommendClass: $(result[5]).text(),
+            recommendInstructor: $(result[6]).text(),
+            studyHours: $(result[7]).text(),
+            avgGradeExpected: $(result[8]).text(),
+            avgGradeReceived: $(result[9]).text(),
+            url: url
+        };
+
+        return callback();
     });
-}
+};

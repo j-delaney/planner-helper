c3f6ce160c56c6a30da81116e136834b97d1cae4
J Delaney <j.delaney.sd@gmail.com>
2015-04-08 14:05:43 -0700

    Made a separate object for the PlannerHelper

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,109 +1,3 @@
-function parseQuery(query) {
-    query = query.substr(1); //Remove the ? at the beginning
-    var vars = query.split('&');
-    var params = {};
-    for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split('=');
-        params[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
-    }
-
-    return params;
-}
-
-function parseCourseID(courseID) {
-    var matches = courseID.match(/([A-Za-z]*)(.*)/);
-    if (matches && matches.length === 3) {
-        return [matches[1], matches[2]];
-    } else {
-        return false;
-    }
-}
-
-function parseTeacherName(teacher) {
-    var matches = teacher.match(/(\w*),\s(\w*)/);
-    if (!matches || matches.length < 3) {
-        return false;
-    }
-    return {
-        fullname: teacher,
-        nomiddle: matches[1] + ', ' + matches[2],
-        fname: matches[2],
-        lname: matches[1]
-    };
-}
-
-function findTeacherName(page) {
-    var teacher = $('#listing').find('ul.courseinfo').children('li').first().text();
-    return parseTeacherName(teacher);
-}
-
-function findClass(params, page, a) {
-    var courseid;
-    if (page === 'Select') {
-        courseid = $(a).text();
-    } else {
-        courseid = params['courseid'];
-    }
-    var result = parseCourseID(courseid);
-    if (!result || result.length !== 2) {
-        console.error('Problem parsing course ID: ' + params);
-        return false;
-    }
-
-    return {
-        subjectCode: result[0],
-        courseCode: result[1]
-    }
-}
-
-var plannerHelper = null;
-
-function formatBase(teacher, course) {
-    plannerHelper.find('h2').text('Planner Helper Data for ' + teacher.fname + ' ' + teacher.lname + ', ' + course.subjectCode + course.courseCode);
-}
-
-/**
- * We can't fetch data from the page until the sidebar with course info is done loading. This function
- * waits until the loading icon is gone (i.e. the new data is done loading) and then calls the callback.
- * @param fn The callback function to call when the page is done loading.
- */
-function waitUntilDoneLoading(fn) {
-    if ($('#loading').is(':hidden')) {
-        fn();
-    } else {
-        setTimeout(function () {
-            return waitUntilDoneLoading(fn);
-        }, 100);
-    }
-}
-
-/**
- * Updates the professor data shown to the user.
- *
- * @param params The params grabbed from the clicked link.
- * @param page The page we're viewing.
- * @param a The link object itself.
- */
-function reloadData(params, page, a, rmp, cape, gradeDist) {
-    //TODO: Refactor this entire thing.
-    waitUntilDoneLoading(function () {
-        var teacher = findTeacherName(page);
-        var course = findClass(params, page, a);
-
-        plannerHelper.find('.yes-data').slideUp(250);
-        plannerHelper.find('.no-data').slideUp(250);
-        plannerHelper.find('.loading-data').slideDown(250);
-
-        formatBase(teacher, course);
-        rmp.updateData(teacher, course, function () {});
-        cape.updateData(teacher, course, function () {});
-        gradeDist.updateData(teacher, course, function () {});
-
-        $('#planner-helper-data').show();
-        $('#planner-helper-nodata').hide();
-    });
-}
-
 $(document).ready(function () {
     var href = window.location.href;
 
@@ -111,41 +5,16 @@ $(document).ready(function () {
         return;
     }
 
-     plannerHelper = $(
-        '<div id="planner-helper">' +
-            '<h2>Planner Helper Data</h2>' +
-            '<div id="planner-helper-data">' +
-            '</div>' +
-            '<div id="planner-helper-nodata">First select a professor to see data about them</div>' +
-        '</div>'
-    );
+    var plannerHelper = new PlannerHelper();
 
-    $('#tdr_content_content').children().first().after(plannerHelper);
-    var rmp = new RMP();
-    var cape = new Cape();
-    var gradeDist = new GradeDist();
+    $('#tdr_content_content').children().first().after(plannerHelper.element);
 
-    plannerHelper.find('#planner-helper-data').append(rmp.elements.main);
-    plannerHelper.find('#planner-helper-data').append(cape.elements.main);
-    plannerHelper.find('#planner-helper-data').append(gradeDist.elements.main);
+    console.log(plannerHelper);
 
     $('#listing').on('click', 'a', function (event) {
-        var a = $(event.target)[0];
-        var params = parseQuery(a.search);
-
-        if ('sectionletter' in params) { //Viewing a teacher
-            reloadData(params, 'Default', a, rmp, cape, gradeDist);
-        }
+        plannerHelper.reloadData(event);
     });
-
     $('#calendar').on('click', 'a', function (event) {
-        var a = $(event.target)[0];
-        var params = parseQuery(a.search);
-
-        if (params.jlinkevent === 'Select') {
-            reloadData(params, 'Select', a, rmp, cape, gradeDist);
-        } else if (params.jlinkevent === 'Subsections') {
-            reloadData(params, 'Default1', a, rmp, cape, gradeDist);
-        }
+        plannerHelper.reloadData(event);
     });
 });

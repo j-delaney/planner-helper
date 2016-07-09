9f12dd9c6b8df1ef0d4f0c247ba687c7beecd515
Cameron Ellis <cellis@ucsd.edu>
2016-07-09 15:39:56 -0700

    just a simple commit change to see if this works

--- src/inject/rmp.js
+++ src/inject/rmp.js
@@ -4,6 +4,7 @@ function RMP(errorHandler) {
         {label: 'Helpfulness', dataField: 'helpfulness'},
         {label: 'Clarity', dataField: 'clarity'},
         {label: 'Easiness', dataField: 'easiness'}
+        // adding a comment here
     ], 'Rate My Professor does not contain an entry for this professor.', errorHandler);
 
     //This is for teachers who have a different name on the class planner and RMP

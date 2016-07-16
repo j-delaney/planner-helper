1b0fb0f5ac8b2eba1a75383a814199acbee1e4da
Cameron Ellis <cellis@ucsd.edu>
2016-07-16 00:42:47 -0700

    snippet on how to extract href

--- src/inject/cape.js
+++ src/inject/cape.js
@@ -44,6 +44,8 @@ Cape.prototype.getNewData = function (teacher, course, callback) {
             this.data = null;
             return callback();
         }
+        var hrefValue = page.find('#ctl00_ContentPlaceHolder1_gvCAPEs_ctl02_hlViewReport').attr('href')
+        console.log("href: " + hrefValue)
 
         this.data = {
             enrolled: $(result[3]).text(),
